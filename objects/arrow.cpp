2820f2f5cb40cf23bcbb1aa1a52e0a9f906daf85
Xayah-Hina <ycxiang14sep@gmail.com>
2023-03-07 22:50:59 +0800

    remove legacy2 & neighbor search

--- objects/arrow.cpp
+++ objects/arrow.cpp
@@ -1,27 +1,17 @@
-//#include "arrow.h"
-//
-//void Kasumi::ArrowObject::INSPECT()
-//{
-//	ObjectMesh3D::INSPECT();
-//
-//	ImGui::Text("init origin: %f %f %f", _opt.origin.x(), _opt.origin.y(), _opt.origin.z());
-//	ImGui::Text("init direction: %f %f %f", _opt.direction.x(), _opt.direction.y(), _opt.direction.z());
-//}
-//Kasumi::ArrowObject::ArrowObject()
-//{
-//	NAME = "Arrow";
-//	MESH = "arrow";
-//	_shader = Shader::DefaultMeshShader;
-//	_init();
-//}
-//void Kasumi::ArrowObject::sync_opt()
-//{
-//	if (!_dirty)
-//		return;
-//
-//	POSE.position = _opt.origin;
-//	POSE.euler = mQuaternion(mVector3(0, 0, 1), _opt.direction).euler();
-//	POSE.scale = mVector3(0.2, 0.3, 0.2);
-//
-//	_dirty = false;
-//}
+#include "arrow.h"
+
+Kasumi::ArrowObject::ArrowObject()
+{
+	NAME = "Arrow";
+	_shader = Shader::DefaultMeshShader;
+	_init("arrow", "");
+}
+void Kasumi::ArrowObject::_update_surface()
+{
+	if (!_dirty)
+	{
+		_origin = POSE.position;
+		_direction = mQuaternion(POSE.euler) * mVector3::UnitY();
+		_dirty = false;
+	}
+}
--- objects/arrow.h
+++ objects/arrow.h
@@ -1,26 +1,24 @@
-//#ifndef HINAPE_ARROW_H
-//#define HINAPE_ARROW_H
-//
-//// Copyright (c) 2023 Xayah Hina
-//// MPL-2.0 license
-//
-//#include "object3D.h"
-//
-//namespace Kasumi
-//{
-//class ArrowObject : public ObjectMesh3D
-//{
-//public:
-//	struct Opt
-//	{
-//		mVector3 origin = mVector3::Zero();
-//		mVector3 direction = mVector3::UnitZ();
-//	} _opt;
-//	void INSPECT() override;
-//	ArrowObject();
-//	void sync_opt() final;
-//};
-//using ArrowObjectPtr = std::shared_ptr<ArrowObject>;
-//} // namespace Kasumi
-//
-//#endif //HINAPE_ARROW_H
+#ifndef HINAPE_ARROW_H
+#define HINAPE_ARROW_H
+
+// Copyright (c) 2023 Xayah Hina
+// MPL-2.0 license
+
+#include "object3D.h"
+
+namespace Kasumi
+{
+class ArrowObject : public ObjectMesh3D
+{
+public:
+	mVector3 _origin = mVector3::Zero();
+	mVector3 _direction = mVector3::UnitZ();
+	ArrowObject();
+
+protected:
+	void _update_surface() final;
+};
+using ArrowObjectPtr = std::shared_ptr<ArrowObject>;
+} // namespace Kasumi
+
+#endif //HINAPE_ARROW_H
