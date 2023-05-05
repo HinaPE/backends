8aae3aa5c9c3f5e559c7869f7b0471c5a31978fe
Xayah-Hina <ycxiang14sep@gmail.com>
2023-05-05 18:58:52 +0800

    [image processing] get & set pixel

--- OpenGL/texture.cpp
+++ OpenGL/texture.cpp
@@ -11,37 +11,50 @@
 
 Kasumi::Texture::Texture(const std::string &path) : _path(path)
 {
-    ID = _width = _height = _nr_channels = 0;
-    unsigned char *data = stbi_load(path.c_str(), &_width, &_height, &_nr_channels, 0);
+	_width = _height = _nr_channels = 0;
+	_data = stbi_load(path.c_str(), &_width, &_height, &_nr_channels, 0);
 
-    if (!data)
-        return;
-
-    auto format = GL_RED;
-    if (_nr_channels == 1)
-        format = GL_RED;
-    else if (_nr_channels == 3)
-        format = GL_RGB;
-    else if (_nr_channels == 4)
-        format = GL_RGBA;
-
-    glGenTextures(1, &ID);
-    glBindTexture(GL_TEXTURE_2D, ID);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, _width, _height, 0, format, GL_UNSIGNED_BYTE, data);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glBindTexture(GL_TEXTURE_2D, 0);
-
-    stbi_image_free(data);
+	update();
 }
 
-Kasumi::Texture::Texture(unsigned char *data, int width, int height, int channels) : _width(width), _height(height), _nr_channels(channels)
-{
-	ID = 0;
+Kasumi::Texture::Texture(unsigned char *data, int width, int height, int channels) : _data(data), _width(width), _height(height), _nr_channels(channels) { update(); }
 
-	if (!data)
+Kasumi::Texture::~Texture()
+{
+	glDeleteTextures(1, &ID);
+	std::cout << "delete texture: " << _path << std::endl;
+}
+
+void Kasumi::Texture::bind(int texture_idx) const
+{
+	switch (texture_idx)
+	{
+		case 0:
+			glActiveTexture(GL_TEXTURE0);
+			break;
+		case 1:
+			glActiveTexture(GL_TEXTURE1);
+			break;
+		case 2:
+			glActiveTexture(GL_TEXTURE2);
+			break;
+		case 3:
+			glActiveTexture(GL_TEXTURE3);
+			break;
+		case 4:
+			glActiveTexture(GL_TEXTURE4);
+			break;
+		case 5:
+			glActiveTexture(GL_TEXTURE5);
+			break;
+		default: // TOO MUCH TEXTURES
+			break;
+	}
+	glBindTexture(GL_TEXTURE_2D, ID);
+}
+void Kasumi::Texture::update()
+{
+	if (!_data)
 		return;
 
 	auto format = GL_RED;
@@ -54,44 +67,32 @@ Kasumi::Texture::Texture(unsigned char *data, int width, int height, int channel
 
 	glGenTextures(1, &ID);
 	glBindTexture(GL_TEXTURE_2D, ID);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, _width, _height, 0, format, GL_UNSIGNED_BYTE, data);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, _width, _height, 0, format, GL_UNSIGNED_BYTE, _data);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	glBindTexture(GL_TEXTURE_2D, 0);
 }
-
-Kasumi::Texture::~Texture()
+auto Kasumi::Texture::get(int x, int y) const -> mVector4
 {
-    glDeleteTextures(1, &ID);
-    std::cout << "delete texture: " << _path << std::endl;
+	// get pixel color from texture data
+	mVector4 color;
+
+	auto red = (int) _data[(_width * y + x) * _nr_channels];
+	auto green = (int) _data[(_width * y + x) * _nr_channels + 1];
+	auto blue = (int) _data[(_width * y + x) * _nr_channels + 2];
+	auto alpha = (int) _data[(_width * y + x) * _nr_channels + 3];
+
+	color = {red, green, blue, alpha};
+
+	return color;
 }
-
-void Kasumi::Texture::bind(int texture_idx) const
+void Kasumi::Texture::set(int x, int y, const mVector4 &pixel)
 {
-    switch (texture_idx)
-    {
-        case 0:
-            glActiveTexture(GL_TEXTURE0);
-            break;
-        case 1:
-            glActiveTexture(GL_TEXTURE1);
-            break;
-        case 2:
-            glActiveTexture(GL_TEXTURE2);
-            break;
-        case 3:
-            glActiveTexture(GL_TEXTURE3);
-            break;
-        case 4:
-            glActiveTexture(GL_TEXTURE4);
-            break;
-		case 5:
-			glActiveTexture(GL_TEXTURE5);
-			break;
-        default: // TOO MUCH TEXTURES
-            break;
-    }
-    glBindTexture(GL_TEXTURE_2D, ID);
+	// set pixel color to texture data
+	_data[(_width * y + x) * _nr_channels] = (unsigned char) pixel.x();
+	_data[(_width * y + x) * _nr_channels + 1] = (unsigned char) pixel.y();
+	_data[(_width * y + x) * _nr_channels + 2] = (unsigned char) pixel.z();
+	_data[(_width * y + x) * _nr_channels + 3] = (unsigned char) pixel.w();
 }
--- texture.h
+++ texture.h
@@ -4,6 +4,8 @@
 // Copyright (c) 2023 Xayah Hina
 // MPL-2.0 license
 
+#include "common.h"
+
 #include <string>
 #include <memory>
 
@@ -17,11 +19,15 @@ public:
 	~Texture();
 
 	void bind(int texture_idx = 0) const;
+	void update();
+	auto get(int x, int y) const -> mVector4;
+	void set(int x, int y, const mVector4& pixel);
 
 public:
-	unsigned int ID;
+	unsigned int ID = 0;
 	std::string _path;
 	int _width, _height, _nr_channels;
+	unsigned char *_data;
 };
 using TexturePtr = std::shared_ptr<Texture>;
 } // namespace Kasumi
